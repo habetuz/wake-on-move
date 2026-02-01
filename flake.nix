89e6526f8ea47e37fe8969f38949050ac796a5cb
Marvin Fuchs <info@marvin-fuchs.de>
2026-02-01 10:43:33 +0100

    switch to bun

--- flake.nix
+++ flake.nix
@@ -35,67 +35,15 @@
             # The Nix packages provided in the environment
             # Add any you need here
             packages = with pkgs; [
-              uv
-              black
-              # System libraries required by OpenCV
-              xorg.libxcb
-              xorg.libX11
-              xorg.libXext
-              libGL
-              glib
-              stdenv.cc.cc.lib
-              # Qt libraries for cv2.imshow (--show-video)
-              qt5.qtbase
-              qt5.qtwayland
-              xorg.libXi
-              xorg.libXrender
-              xorg.libXrandr
-              xorg.libXcursor
-              xorg.libXinerama
-              xorg.libXtst
-              xorg.xcbutil
-              xorg.xcbutilimage
-              xorg.xcbutilkeysyms
-              xorg.xcbutilrenderutil
-              xorg.xcbutilwm
-              fontconfig
-              freetype
+              bun
             ];
 
             # Set any environment variables for your dev shell
             env = {
-              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-                pkgs.stdenv.cc.cc
-                pkgs.xorg.libxcb
-                pkgs.xorg.libX11
-                pkgs.xorg.libXext
-                pkgs.libGL
-                pkgs.glib
-                pkgs.qt5.qtbase
-                pkgs.qt5.qtwayland
-                pkgs.xorg.libXi
-                pkgs.xorg.libXrender
-                pkgs.xorg.libXrandr
-                pkgs.xorg.libXcursor
-                pkgs.xorg.libXinerama
-                pkgs.xorg.libXtst
-                pkgs.xorg.xcbutil
-                pkgs.xorg.xcbutilimage
-                pkgs.xorg.xcbutilkeysyms
-                pkgs.xorg.xcbutilrenderutil
-                pkgs.xorg.xcbutilwm
-                pkgs.fontconfig
-                pkgs.freetype
-              ];
-              QT_PLUGIN_PATH = "${pkgs.qt5.qtbase}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
-              QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt5.qtbase}/lib/qt-${pkgs.qt5.qtbase.version}/plugins/platforms";
-              # Force X11 backend for Qt/OpenCV
-              QT_QPA_PLATFORM = "xcb";
             };
 
             # Add any shell logic you want executed any time the environment is activated
             shellHook = ''
-              echo "Development environment loaded with OpenCV dependencies"
             '';
           };
         }
